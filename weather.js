3130fd39c57fb2b9e44b7922489f62e6201b49a6
umarmunir449 <118309030+umarmunir449@users.noreply.github.com>
2023-01-15 23:55:41 -0800

    Initial commit

---
+++ weather.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+//current weather
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '3ec9033f03msh62d4b617fd88a55p19ba24jsndd412a4c34c4',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getweather = (city)  =>{
+    cityName.innerHTML= city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+    .then(response => response.json())
+    .then(response => {
+
+
+        console.log(response)
+
+        
+cloud_pct.innerHTML =response.cloud_pct
+temp.innerHTML = response.temp
+temp2.innerHTML = response.temp
+
+feels_like.innerHTML =response.feels_like
+humidity.innerHTML =response.humidity
+humidity2.innerHTML =response.humidity
+min_temp.innerHTML =response.min_temp
+max_temp.innerHTML =response.max_temp
+wind_speed.innerHTML =response.wind_speed
+wind_speed2.innerHTML =response.wind_speed
+wind_degrees.innerHTML =response.wind_degrees
+sunrise.innerHTML =response.sunrise
+sunset.innerHTML =response.sunset
+wind_degrees.innerHTML =response.wind_degrees
+
+
+    })
+    .catch (err => console.error(err));
+}
+    submit.addEventListener("click",(e)=>{
+        e.preventDefault()
+        getweather(city.value)  
+    })
+getweather(lahore);
